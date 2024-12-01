400a9c60b59489fba57e12b9bdd0cae9de0cb947
Akshay7016 <asajgule@gmail.com>
2024-12-01 19:38:46 +0530

    LinkedList created and push,pop methods implemented

--- 4. LinkedList/1. Singly LinkedList/index.js
+++ 4. LinkedList/1. Singly LinkedList/index.js
@@ -1,9 +1,9 @@
 class Node {
   constructor(value) {
-    this.head = value;
+    this.data = value;
     this.next = null;
   }
-}
+};
 
 class LinkedList {
   constructor(value) {
@@ -13,13 +13,15 @@ class LinkedList {
   }
 
   push(value) {
-    let newNode = new Node(value);
+    const newNode = new Node(value);
 
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
+      return;
     }
 
+    // point previous node to newly created node
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
@@ -27,19 +29,16 @@ class LinkedList {
 
   pop() {
     if (!this.head) {
-      return undefined;
-    }
+      return "No head node found";
+    };
 
-    let temp = this.head;
-    let prev = this.head;
+    let temp = this.head, prev = this.head;
 
     while (temp.next) {
-      // console.log("********** temp", temp);
       prev = temp;
-      temp = prev.next;
+      temp = temp.next;
     }
 
-    // console.log(prev);
     this.tail = prev;
     this.tail.next = null;
     this.length--;
@@ -51,127 +50,14 @@ class LinkedList {
 
     return temp;
   }
+};
 
-  unshift(value) {
-    const newNode = new Node(value);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    }
-
-    newNode.next = this.head;
-    this.head = newNode;
-    this.length++;
-    return this;
-  }
-
-  shift() {
-    if (!this.head) {
-      return undefined;
-    }
-
-    // 1. Point to the first node/element
-    let temp = this.head;
-    // 2. Move the head to the next node/element
-    this.head = this.head.next;
-    // 3. Remove first element
-    temp.next = null;
-    this.length--;
-
-    // If we have one node in the list
-    if (this.length === 0) {
-      this.tail = null;
-    }
-
-    return temp;
-  }
-
-  getFirst() {
-    return this.head;
-  }
-
-  getLast() {
-    if (!this.head) {
-      return null;
-    }
-
-    let node = this.head;
-
-    while (node) {
-      // console.log("***********", node);
-      if (!node.next) {
-        return node;
-      }
-      node = node.next;
-    }
-  }
-
-  get(index) {
-    let counter = 0;
-    let node = this.head;
-
-    while (node) {
-      if (counter === index) {
-        return node;
-      }
-
-      counter++;
-      node = node.next;
-    }
-
-    return null;
-  }
-
-  set(index, value) {
-    let temp = this.get(index);
-    console.log("----------", temp);
-
-    if (temp) {
-      temp.value = value;
-      return true;
-    }
-
-    return false;
-  }
-
-  insert(index, value) {
-    if (index === 0) {
-      return this.unshift(value);
-    }
-
-    if (index === this.length) {
-      return this.push(value);
-    }
-
-    const newNode = new Node(value);
-    // Uses the get method to find the node right before the desired position (index - 1).
-    const temp = this.get(index - 1);
-
-    newNode.next = temp.next;
-    temp.next = newNode;
-    this.length++;
-    return true;
-  }
-
-  size() {
-    let counter = 0;
-    let node = this.head;
-
-    while (node) {
-      counter++;
-      node = node.next;
-    }
-
-    return counter;
-  }
-
-  clear() {
-    this.head = null;
-  }
-}
-
-const myLinkedList = new LinkedList(1);
-myLinkedList.push(2);
-myLinkedList.push(3);
+const myLinkedList = new LinkedList(10);
+myLinkedList.push(20);
+myLinkedList.push(30);
+console.log(myLinkedList)
+const deletedNode1 = myLinkedList.pop();
+const deletedNode2 = myLinkedList.pop();
+const deletedNode3 = myLinkedList.pop();
 console.log(myLinkedList);
+
