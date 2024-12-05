559f4dd085c8aa19c86095520115bbb23ce9e12f
Akshay7016 <asajgule@gmail.com>
2024-12-05 20:53:53 +0530

    Doubly Linked List implemented

--- 4. LinkedList/2. Doubly LinkedList/index.js
+++ 4. LinkedList/2. Doubly LinkedList/index.js
@@ -1,6 +1,6 @@
 class Node {
   constructor(value) {
-    this.value = value;
+    this.data = value;
     this.next = null;
     this.prev = null;
   }
@@ -8,8 +8,7 @@ class Node {
 
 class DoublyLinkedList {
   constructor(value) {
-    const newNode = new Node(value);
-    this.head = newNode;
+    this.head = new Node(value);
     this.tail = this.head;
     this.length = 1;
   }
@@ -20,19 +19,21 @@ class DoublyLinkedList {
     if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      newNode.prev = this.tail;
-      this.tail = newNode;
+      this.length++;
+      return;
     }
 
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
     this.length++;
+
     return this;
   }
 
   pop() {
-    if (this.length === 0) {
-      return undefined;
+    if (!this.head) {
+      return "Unable to delete node";
     }
 
     let temp = this.tail;
@@ -40,35 +41,38 @@ class DoublyLinkedList {
     if (this.length === 1) {
       this.head = null;
       this.tail = null;
-    } else {
-      this.tail = this.tail.prev;
-      this.tail.next = null;
-      temp.prev = null;
+      this.length = 0;
+      return temp;
     }
 
+    this.tail = this.tail.prev;
+    this.tail.next = null;
+    temp.prev = null;
     this.length--;
+
     return temp;
   }
 
   unshift(value) {
     const newNode = new Node(value);
 
-    if (this.length === 0) {
+    if (!this.head) {
       this.head = newNode;
       this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head.prev = newNode;
-      this.head = newNode;
+      this.length++;
+      return this;
     }
 
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
     this.length++;
     return this;
   }
 
   shift() {
-    if (this.length === 0) {
-      return undefined;
+    if (!this.head) {
+      return "Unable to delete node";
     }
 
     let temp = this.head;
@@ -76,18 +80,20 @@ class DoublyLinkedList {
     if (this.length === 1) {
       this.head = null;
       this.tail = null;
-    } else {
-      this.head = this.head.next;
-      this.head.prev = null;
-      temp.next = null;
+      this.length = 0;
+      return temp;
     }
+
+    this.head = this.head.next;
+    this.head.prev = null;
+    temp.next = null;
     this.length--;
+
     return temp;
   }
 }
 
-let myDoublyLinkedList = new DoublyLinkedList(0);
-myDoublyLinkedList.push(1);
-// myDoublyLinkedList.pop();
-// myDoublyLinkedList.shift();
+const myDoublyLinkedList = new DoublyLinkedList(10);
+myDoublyLinkedList.push(20);
+console.log(myDoublyLinkedList.shift());
 console.log(myDoublyLinkedList);
